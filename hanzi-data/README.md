10fe3e07ee3855307e274af00d6208667c8f2ef2
Feihong Hsu <feihong.hsu@gmail.com>
2021-10-25 22:01:28 -0500

    Only pick from top 4000 characters in HanziPyramid

--- hanzi-data/README.md
+++ hanzi-data/README.md
@@ -2,3 +2,7 @@ Setting the levels based on number of occurrences in other words doesn't work. F
 ranked 3980! It's probably more useful to get character lists based on frequency. Here's an article that might help with
 that:
 https://www.hackingchinese.com/the-most-common-chinese-words-characters-and-components-for-language-learners-and-teachers/
+
+I ended up downloading the character frequency list from http://crr.ugent.be/programs-data/subtitle-frequencies/subtlex-ch
+and converting it to CSV. I generated a list ordinal ranges, but all the ranges were very small and I ended up with 2780
+such ranges, so it made more sense to simply list all characters.
--- hanzi-data/analyze_subtlex.py
+++ hanzi-data/analyze_subtlex.py
@@ -6,5 +6,28 @@ def get_chars():
       c, _ = line.split(',', 1)
       yield c
 
-for i, c in enumerate(itertools.islice(get_chars(), 4000), 1):
-  print(i, c)
+top_chars = itertools.islice(get_chars(), 4000)
+
+# for i, c in enumerate(top_chars, 1):
+#   print(i, c)
+
+# top_ords = [ord(c) for c in top_chars]
+# top_ords.sort()
+
+# ranges = [(top_ords[0], top_ords[0])]
+
+# for o in top_ords[1:]:
+#   last_range = ranges[-1]
+#   if o == last_range[1] + 1:
+#     ranges[-1] = (last_range[0], o)
+#   else:
+#     ranges.append((o, o))
+
+# for (i, (s, e)) in enumerate(ranges, 1):
+#   print(i, end='. ')
+#   if s == e:
+#     print(s)
+#   else:
+#     print(s, e, f'{e - s + 1}')
+
+print(''.join(top_chars))
